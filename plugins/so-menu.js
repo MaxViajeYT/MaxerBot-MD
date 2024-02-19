79d74f46e2952a77745d60871b9df7082bec8ccf
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-02-19 04:17:56 -0600

    Update so-menu.js

--- plugins/so-menu.js
+++ plugins/so-menu.js
@@ -5,12 +5,12 @@ if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 100 
 global.db.data.users[m.sender].exp += 100
   
-let vn = './media/el poder corre asta en mis huevos.mp3'
+let vn = './media/menu.mp3'
 conn.sendFile(m.chat, vn, 'el poder corre asta en mis huevos.mp3', null, m, true, { 
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /el poder corre asta en mis huevos|elpodercorreastaenmishuevos/
-handler.command = /^(elpodercorreastaenmishuevos$)/
+handler.customPrefix = /audioefectomenu|buscarmenu|hornymenu|listaporno|listhorny|convertidormenu|descargasmenu|juegosmenu|grupomenu|herramientasmenu|infomenu|makermenu|menulogos2|ownermenu|randommenu|rpgmenu|stickermenu|menu18|menugrupo|menu3|musica|menú3|memu3|menumusica|menumusicas|memumusicas|memumusica|musicas|música|menucompleto|allmenu|menu|menú|memu|memú|help|menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|audio/
+handler.command = /^(audioefectomenu|buscarmenu|hornymenu|listaporno|listhorny|convertidormenu|descargasmenu|juegosmenu|grupomenu|herramientasmenu|infomenu|makermenu|menulogos2|ownermenu|randommenu|rpgmenu|stickermenu|menu18|menugrupo|menu3|musica|menú3|memu3|menumusica|menumusicas|memumusicas|memumusica|musicas|música|menucompleto|allmenu|menu|menú|memu|memú|help|menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|audio$)/
 export default handler
