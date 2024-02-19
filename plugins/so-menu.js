89054a7cff7296485ea215a781e5dbe824bccabe
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-02-19 04:11:36 -0600

    Add files via upload

---
+++ plugins/so-menu.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+global.db.data.users[m.sender].money += 100 
+global.db.data.users[m.sender].exp += 100
+  
+let vn = './media/el poder corre asta en mis huevos.mp3'
+conn.sendFile(m.chat, vn, 'el poder corre asta en mis huevos.mp3', null, m, true, { 
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /el poder corre asta en mis huevos|elpodercorreastaenmishuevos/
+handler.command = /^(elpodercorreastaenmishuevos$)/
+export default handler
