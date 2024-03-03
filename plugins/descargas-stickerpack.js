943964d3c0deb8cbcd0a8258bc56b259bb6a9f73
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-03-03 09:57:30 -0600

    𝙈𝙖𝙭𝙚𝙧𝘽𝙤𝙩-𝙈𝘿

--- plugins/descargas-stickerpack.js
+++ plugins/descargas-stickerpack.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused8}\n${usedPrefix + command} https://getstickerpack.com/stickers/flork-memes-4-1`
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused8}\n${usedPrefix + command} https://getstickerpack.com/stickers/alastor`
 try {  
 let url = text
 let res = await fetch(`https://api.akuari.my.id/downloader/stickerpack?link=${url}`)
@@ -13,9 +13,11 @@ await delay(5000)
 }} catch (e) {
 await m.reply(lenguajeGB['smsMalError3']() + '\n*' + lenguajeGB.smsMensError1() + '*\n*' + usedPrefix + `${lenguajeGB.lenguaje() == 'es' ? 'reporte' : 'report'}` + '* ' + `${lenguajeGB.smsMensError2()} ` + usedPrefix + command)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)}  
-}
+console.log(e)
+handler.limit = false
+}}
 handler.command = /^stickerpack$/i
 handler.limit = 1
+handler.register = true
 export default handler
 const delay = time => new Promise(res => setTimeout(res, time))
