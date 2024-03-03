c3553697104084d794a37f9d1a8f8932a9daa369
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-03-03 01:05:42 -0600

    𝙈𝙖𝙭𝙚𝙧𝘽𝙤𝙩-𝙈𝘿

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -15,13 +15,9 @@ conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 
 }
 
-/*if (/^Mande porno|porno|paja$/i.test(m.text) ) { //sin prefijo 
-    let teks = `
-${pickRandom([`no puedo esta contra las política del grupo.😶`, `_uff miren un pajero_`, `_pagame y paso mi pack😏🥵_`, `_que_`, `_que quiere pija dice 🤣_`, `_pasa el pack de tu hermana😏_`, `_mire un gilipolla_`, `_siuuu sexo sexo sexo😈_`, '_callate putito_'])}
-`.trim()
-conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
-
-}*/
+if (/^Mande porno|porno|paja$/i.test(m.text) ) { //sin prefijo 
+    let teks = `${pickRandom([`no puedo esta contra las política del grupo.😸`, `_uff miren un pajero_`, `_pagame y paso mi pack😏🥵_`, `_que_`, `_que quiere pija dice 🤣_`, `_pasa el pack de tu hermana😏_`, `_mire un gilipolla_`, `_siuuu sexo sexo sexo😈_`, '_callate putito_'])}`.trim()conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})}
+}
 
 if (/^reglas|normas|Reglas|MaxerBot Normas$/i.test(m.text) ) { //sin prefijo 
     conn.reply(m.chat, `╭┅〘 ⚠️ 𝗢𝗯𝗲𝗱𝗲𝗰𝗲 𝗹𝗮𝘀 𝗿𝗲𝗴𝗹𝗮𝘀 ⚠️ 〙*
@@ -33,10 +29,10 @@ if (/^reglas|normas|Reglas|MaxerBot Normas$/i.test(m.text) ) { //sin prefijo
 
 }
 
-/* if (/^hola|halo|hello|hi$/i.test(m.text) ) { //sin prefijo 
+ if (/^hola|halo|hello|hi$/i.test(m.text) ) { //sin prefijo 
      conn.reply(m.chat, `Hola :D`, fkontak, m)
 
- }*/
+ }
  
 if (/^¿que es un bot?|Que es un bot|Que es maxerbot?$/i.test(m.text) ) { //sem prefixo
     conn.reply(m.chat, `╭┄〔 *${wm}* 〕┄⊱
