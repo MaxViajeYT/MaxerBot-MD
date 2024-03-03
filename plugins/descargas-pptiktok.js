be4400043bf3cb5c958f41d39477c5a13395c918
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-03-03 09:49:47 -0600

    𝙈𝙖𝙭𝙚𝙧𝘽𝙤𝙩-𝙈𝘿

--- plugins/descargas-pptiktok.js
+++ plugins/descargas-pptiktok.js
@@ -1,24 +1,26 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, text, command, usedPrefix }) => {
-if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.TikTok}\n*${usedPrefix + command} Gata_Dios*`
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}${mid.TikTok}\n*${usedPrefix + command} maxviajeytgt*`
 try {
 let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`
 await conn.sendFile(m.chat, res, 'error.jpg', `✅ ${mid.TikTok1}\n💟 *${text}*`, m, false)
 conn.reply(m.chat, `${lenguajeGB['smsAvisoIIG']()} *${mid.smsinfo}*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+title: '𝙈𝙖𝙭𝙚𝙧𝘽𝙤𝙩-𝙈𝘿 | ඩා爪Δメモメモ⃟⃢🇲🇽⏤͟͟͞͞✰',
 body: 'Super Bot WhatsApp',         
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
 sourceUrl: md}}}) 
 } catch (e) {
 await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)}}
+console.log(e)
+handler.limit = false
+}}
 handler.help = ['tiktokfoto'].map(v => v + ' <username>')
 handler.tags = ['downloader']
 handler.command = /^(tiktokfoto|tiktokphoto)$/i
 handler.limit = 1
-handler.exp = 68
+handler.register = true
 export default handler
 
 /*conn.sendHydrated(m.chat, info, wm, null, md, '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
