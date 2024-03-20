043f480da415d4c48b020d59ead535ee2aa0ff05
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-03-20 00:54:01 -0600

    𝙈𝙖𝙭𝙚𝙧𝘽𝙤𝙩-𝙈𝘿

--- main.js
+++ main.js
@@ -194,7 +194,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['MaxerBot-MD', 'Pornhub', '2.0.0'] : methodCodeQR ? ['MaxerBot-MD', 'Pornhub', '2.0.0'] : ['MaxerBot, 'Pornhub', '110.0.1587.56'],
+browser: opcion == '1' ? ['MaxerBot-MD', 'Edge, '2.0.0'] : methodCodeQR ? ['MaxerBot-MD', 'Edge', '2.0.0'] : ['Ubuntu, 'Edge', '110.0.1587.56'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
