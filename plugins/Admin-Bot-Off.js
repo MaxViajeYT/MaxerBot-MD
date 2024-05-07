f143910561f5fcc0a3209df2246befd66ffe2f02
MaxViajeYT <113333355+MaxViajeYT@users.noreply.github.com>
2024-05-07 11:03:13 -0600

    Update Admin-Bot-Off.js

--- plugins/Admin-Bot-Off.js
+++ plugins/Admin-Bot-Off.js
@@ -1,11 +1,12 @@
 /*let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
+m.reply(`${eg}𝙎𝙀 𝙋𝙍𝙀𝙉𝘿𝙄𝙊 𝙀𝙇 𝘽𝙊𝙏 𝙀𝙉 𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙀 𝘽𝙊𝙏 𝙒𝘼𝙎 𝙏𝙐𝙍𝙉𝙀𝘿 𝙊𝙉 𝙄𝙉 𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
 }
-handler.help = ['unbanchat2']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.rowner = true
+handler.help = ['bot-off/on']
+handler.tags = ['admin']
+handler.command = /^bot-on|maxer-on|bon|mon$/i
+handler.rowner = false
+handler.admin = true
 handler.exp = 50
 
 export default handler*/
@@ -21,5 +22,5 @@ handler.tags = ['admin']
 handler.command = /^bot-off|maxer-off|boff|moff$/i
 handler.botAdmin = false
 handler.admin = true
-hanler.rowmer = false
+handler.rowmer = false
 export default handler
